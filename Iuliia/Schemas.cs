f53670ec267569d1e6d5ca80ab398eba5dd8d417
Belianin <belyaninwork@gmail.com>
2020-05-01 11:35:34 +0500

    All the Schemas are working

--- Iuliia/Schemas.cs
+++ Iuliia/Schemas.cs
@@ -45,6 +45,27 @@ namespace Iuliia
             YandexMoney = LoadScheme("yandex_money.json");
             Telegram = LoadScheme("telegram.json");
             Scientific = LoadScheme("scientific.json");
+            ALA_LC = LoadScheme("ala_lc.json");
+            ALA_LC_ALT = LoadScheme("ala_lc_alt.json");
+            BGN_PCGN = LoadScheme("bgn_pcgn.json");
+            BGN_PCGN_ALT = LoadScheme("bgn_pcgn_alt.json");
+            BS_2979 = LoadScheme("bs_2979.json");
+            BS_2979_ALT = LoadScheme("bs_2979_alt.json");
+            GOST_16876 = LoadScheme("gost_16876.json");
+            GOST_16876_ALT = LoadScheme("gost_16876_alt.json");
+            GOST_52290 = LoadScheme("gost_52290.json");
+            GOST_52535 = LoadScheme("gost_52535.json");
+            GOST_7034 = LoadScheme("gost_7034.json");
+            GOST_779 = LoadScheme("gost_779.json");
+            GOST_779_ALT = LoadScheme("gost_779_alt.json");
+            ICAO_DOC_9303 = LoadScheme("icao_doc_9303.json");
+            ISO_9_1954 = LoadScheme("iso_9_1954.json");
+            ISO_9_1968 = LoadScheme("iso_9_1968.json");
+            ISO_9_1968_ALT = LoadScheme("iso_9_1968_alt.json");
+            MVD_310 = LoadScheme("mvd_310.json");
+            MVD_310_FR = LoadScheme("mvd_310_fr.json");
+            MVD_782 = LoadScheme("mvd_782.json");
+            UNGEGN_1987 = LoadScheme("ungegn_1987.json");
         }
 
         private static Scheme LoadScheme(string filename)
